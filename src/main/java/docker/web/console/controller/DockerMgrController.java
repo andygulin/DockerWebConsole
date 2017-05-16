0838dd41cbc5534e64741fe6f481b1de15022c7c
andygulin <andygulin@hotmail.com>
2017-05-16 11:56:27 +0800

    版本更新...

--- src/main/java/docker/web/console/controller/DockerMgrController.java
+++ src/main/java/docker/web/console/controller/DockerMgrController.java
@@ -10,7 +10,11 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/mgr")
